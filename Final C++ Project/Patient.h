66d6801dd6f983d28818ad0c30b1b33b20f84e59
GabrielR008 <150851389+GabrielR008@users.noreply.github.com>
2023-11-29 14:13:54 -0500

    Add files via upload

---
+++ Final C++ Project/Patient.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Patient {
+
+friend class Surgery;
+
+
+private:
+	float charges;
+	int days;
+	const float dayRate = 1250;
+	string fname;
+	string lname;
+
+public:
+
+	//Constructor
+	Patient() {
+		charges = 0;
+		days = 0;
+		fname = "";
+		lname = "";
+
+		}
+
+
+	//Setters
+	void setName() {
+
+
+		cout << "Enter your patient's first name:" << endl;
+		cin >> fname;
+		cout << "Enter your patient's last name:" << endl;
+		cin >> lname;
+
+	}
+
+	friend void addCharge(Patient &p, float c);
+	
+	int addDays(Patient &p) {
+    bool flag = true;
+
+    while(flag){
+      int d;
+      cout << "How many days did your patient spend in the hospital?"<<endl;
+      cin>>d;
+      if (d > 0) {
+		    days += d;
+        flag = false;
+        addCharge(p,days*dayRate);
+      }
+      else{
+        cout<<"Not a valid number of days, please try again"<<endl;
+        
+      }
+
+	}
+    return days;
+  }
+
+
+	//Getters
+
+	float getCharges() {
+		return charges;
+
+	}
+
+	string getName() {
+	 return lname+", "+fname;
+
+	}
+
+  int getDays(){
+   return days;
+    
+  }
+
+  int getDayCost(){
+    return days*dayRate;
+  }
+
+
+    friend class Pharmacy;
+    friend class Surgery;
+};
+
+void addCharge(Patient &p, float c){
+  p.charges+=c;
+  
+}
